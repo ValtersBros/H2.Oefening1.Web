c901a9675cf0a63cb62edb9089c1642ade50cacf
ValtersBros <valter@hotmail.be>
2019-09-24 15:49:15 +0200

    Finalize HomeController

--- H2.Oefening1.Web/Controllers/HomeController.cs
+++ H2.Oefening1.Web/Controllers/HomeController.cs
@@ -8,16 +8,25 @@ namespace H2.Oefening1.Web.Controllers
 {
     public class HomeController : Controller
     {
-       
-        public IActionResult Index()
-        {   
-            return View();
-        }
-
         public IActionResult WhatsMyName()
         {
             //https://localhost:44349/Home/WhatsMyName
-            return Content("Valters Lukass Brosinskis - " + DateTime.Today);
+
+            string message = $"My name is: Valters Lukass Brosinskis, Today's date is: {DateTime.Now}";
+            return Content(message, "text/plain");
+        }
+
+        public IActionResult Index()
+        {
+            string message = "<url>" + "<ul><li><a href='https://www.youtube.com/feed/subscriptions'>Youtube</a></li>"
+                                    + "<li><a href='https://www.meteovista.be/Europa/Belgie/Nieuwpoort/4054553'>Weeronline</a></li>"
+                                    + "<li><a href='https://www.facebook.com/'>Facebook</a></li></ul>";
+
+            /*
+            stringBuilder stringBuilder = new StringBuilder();
+            */
+
+            return Content(message, "text/html");
         }
     }
 }
