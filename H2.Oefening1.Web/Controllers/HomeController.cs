6e3c8b82533e0483d15b3338bd71efd1b33f27e2
ValtersBros <valter@hotmail.be>
2019-09-24 14:52:04 +0200

    Create repository

---
+++ H2.Oefening1.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2.Oefening1.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WhatsMyName()
+        {
+            return Content("Valters Lukass Brosinskis - " + DateTime.Today);
+            
+        }
+    }
+}
---
+++ H2.Oefening1.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace H2.Oefening1.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
